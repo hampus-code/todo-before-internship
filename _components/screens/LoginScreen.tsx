b0deb2c45b9fb8d49cc48facba43a97c7a8138ef
hampus-code <hampusss96@gmail.com>
2025-01-06 13:45:28 +0100

    Add: Loginform that includes textinput, custombutton and pressable text to navigate

--- _components/screens/LoginScreen.tsx
+++ _components/screens/LoginScreen.tsx
@@ -1,9 +1,33 @@
-import { View, Text } from "react-native"
+import { View, Text, TextInput, Pressable } from "react-native"
+import CustomButton from "../buttons/CustomButton"
+import { HomeScreenProps, LoginScreenProps } from "../../_type/IProps"
 
-export default function LoginScreen() {
+export default function LoginScreen({ navigation }: LoginScreenProps) {
   return (
-    <View>
-      <Text>This is LoginScreen</Text>
+    <View className="items-center justify-center pt-60">
+      <View className="items-center justify-center p-10 border rounded-3xl">
+        <Text className="font-bold text-lg">Login</Text>
+        <View className="items-center justify-center pt-10">
+          <TextInput
+            className="border rounded w-48 h-7 p-1"
+            placeholder="Username"
+            placeholderTextColor="black"
+          ></TextInput>
+          <TextInput
+            className="border rounded w-48 h-7 p-1 mt-4 mb-4"
+            placeholder="Password"
+            placeholderTextColor="black"
+          ></TextInput>
+          <CustomButton title="Login" onPress={() => {}}></CustomButton>
+        </View>
+        <View className="items-center justify-center pt-10">
+          <Pressable onPress={() => navigation.navigate("Register")}>
+            <Text className="underline">
+              Don't have an account? Register account
+            </Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   )
 }
